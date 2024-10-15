1023817da8f0dc1e397ab06c336213ca97d0d3ea
Joshua Farayola <farayolajoshua@gmail.com>
2024-10-15 14:10:09 +0100

    Remove unused deps

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
@@ -10,8 +9,3 @@ root.render(
     <App />
   </StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
